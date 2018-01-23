PUBDEV-5008: ordinal glm.  Fixed bug in calculating gradient multiplier. PUBDEV-5008: ordinal glm.  Added ordinal as new family in GLM, added link ologit, oprobit, ologlog as well for ordinal GLM regression. PUBDEV-5008: ordinal glm.  Added pyunit test for ordinal GLM regression. PUBDEV-5008: ordinal glm.  Added ordinal as valid distribution family for GLM. PUBDEV-5008: ordinal glm.  Added ordinal into schemas.  Added Junit test.  Added ordinal as an option whenever I can. PUBDEV-5008: ordinal glm.  Added threshold initalization. PUBDEV-5008: ordinal glm.  Added link function, linkderi, linkinv. PUBDEV-5008: ordinal glm.  Calculate gradient with ordinal regression ologit link. PUBDEV-5008: ordinal glm.  Finished debugging coordinate descent maunally. PUBDEV-5008: ordinal glm.  Fixed auto-gen binding for glm to include ordinal. PUBDEV-5008: ordinal glm.  Added gradient descent, not coordinate descend. PUBDEV-5008: ordinal glm.  Added mojo/pojo test for glm ordinal regression. PUBDEV-5008: ordinal glm.  Added schema to water.api.Schema, fixed genModel.java I hope and added Runit. PUBDEV-5008: ordinal glm.  Fixed resDOF() calculation for ordinal regression. PUBDEV-5008: ordinal regression.  Add errors for bad solvers or operations not supported by ordinal regression. PUBDEV-5008: ordinal regression.  Completed gradient check between ordinal regression and Tomas binomial regression. PUBDEV-5008: ordinal glm.  Fixed coefficient updates junit test.

h2o-py/tests/pyunit_utils/utilsPY.py
@@ -23,6 +23,7 @@ from h2o.utils.shared_utils import temp_ctr
 from h2o.model.binomial import H2OBinomialModel
 from h2o.model.clustering import H2OClusteringModel
 from h2o.model.multinomial import H2OMultinomialModel
+from h2o.model.ordinal import H2OOrdinalModel
 from h2o.model.regression import H2ORegressionModel
 from h2o.estimators.gbm import H2OGradientBoostingEstimator
 from h2o.estimators.deeplearning import H2ODeepLearningEstimator
@@ -82,7 +83,7 @@ def check_models(model1, model2, use_cross_validation=False, op='e'):
                                             "{1}. Expected the first to be > than the second.".format(mse1, mse2)
         elif op == 'ge': assert mse1 >= mse2, "The first model has an MSE of {0} and the second model has an MSE of " \
                                               "{1}. Expected the first to be >= than the second.".format(mse1, mse2)
-    elif isinstance(model1,H2OMultinomialModel): #   2c. Multinomial
+    elif isinstance(model1,H2OMultinomialModel) or isinstance(model1,H2OOrdinalModel): #   2c. Multinomial
         # hit-ratio
         pass
     elif isinstance(model1,H2OClusteringModel): #   2d. Clustering
@@ -739,7 +740,8 @@ def generate_weights_glm(csv_weight_filename, col_count, data_type, min_w_value,
             weight = np.random.uniform(min_w_value, max_w_value, [col_count+1, 1])
         else:
             assert False, "dataType must be 1 or 2 for now."
-    elif ('binomial' in family_type.lower()) or ('multinomial' in family_type.lower()):
+    elif ('binomial' in family_type.lower()) or ('multinomial' in family_type.lower()
+                                                 or ('ordinal' in family_type.lower())):
         if 'binomial' in family_type.lower():  # for binomial, only need 1 set of weight
             class_number -= 1
 
@@ -758,7 +760,23 @@ def generate_weights_glm(csv_weight_filename, col_count, data_type, min_w_value,
         else:
             assert False, "dataType must be 1 or 2 for now."
 
+    # special treatment for ordinal weights
+    if 'ordinal' in family_type.lower():
+        # make sure the intercepts are increasing in value
+
+        # make sure betas for all classes are the same
     # save the generated intercept and weight
+        intercepts = [] # row is predictor, col is class
+        for classIndex in list(range(class_number)):
+            intercepts.append(weight[0, classIndex])
+            if classIndex > 0:
+                for predIndex in list(range(1,temp_col_count+1)):
+                    weight[predIndex, classIndex]=weight[predIndex,0]
+
+        intercepts.sort()
+        for index in range(class_number):
+            weight[0,index] = intercepts[index]
+
     np.savetxt(csv_weight_filename, weight.transpose(), delimiter=",")
     return weight
 
@@ -811,7 +829,7 @@ def generate_training_set_glm(csv_filename, row_count, col_count, min_p_value, m
 
     # for family_type = 'multinomial' or 'binomial', response_y can be -ve to indicate bad sample data.
     # need to delete this data sample before proceeding
-    if ('multinomial' in family_type.lower()) or ('binomial' in family_type.lower()):
+    if ('multinomial' in family_type.lower()) or ('binomial' in family_type.lower()) or ('ordinal' in family_type.lower()):
         if 'threshold' in class_method.lower():
             if np.any(response_y < 0):  # remove negative entries out of data set
                 (x_mat, response_y) = remove_negative_response(x_mat, response_y)
@@ -1135,7 +1153,7 @@ def generate_response_glm(weight, x_mat, noise_std, family_type, class_method='p
     """
     (num_row, num_col) = x_mat.shape
 
-    # add a column of 1's to x_mat
+    # add a column of 1's to x_mat, intercept as first element
     temp_ones_col = np.asmatrix(np.ones(num_row)).transpose()
     x_mat = np.concatenate((temp_ones_col, x_mat), axis=1)
 
@@ -1143,20 +1161,24 @@ def generate_response_glm(weight, x_mat, noise_std, family_type, class_method='p
     response_y = x_mat * weight + noise_std * np.random.standard_normal([num_row, 1])
 
     # added more to form Multinomial response
-    if ('multinomial' in family_type.lower()) or ('binomial' in family_type.lower()):
+    if ('multinomial' in family_type.lower()) or ('binomial' in family_type.lower()
+                                                  or ('ordinal' in family_type.lower())):
         temp_mat = np.exp(response_y)   # matrix of n by K where K = 1 for binomials
+        if ('ordinal' in family_type.lower()):
+            temp_den = temp_mat+1
+            temp_mat = temp_mat/temp_den
 
         if 'binomial' in family_type.lower():
             ntemp_mat = temp_mat + 1
             btemp_mat = temp_mat / ntemp_mat
             temp_mat = np.concatenate((1-btemp_mat, btemp_mat), axis=1)    # inflate temp_mat to 2 classes
 
-        response_y = derive_discrete_response(temp_mat, class_method, class_margin)
+        response_y = derive_discrete_response(temp_mat, class_method, class_margin, family_type)
 
     return response_y
 
 
-def derive_discrete_response(prob_mat, class_method, class_margin):
+def derive_discrete_response(prob_mat, class_method, class_margin, family_type='binomial'):
     """
     This function is written to generate the final class response given the probabilities (Prob(y=k)).  There are
     two methods that we use and is specified by the class_method.  If class_method is set to 'probability',
@@ -1175,11 +1197,18 @@ def derive_discrete_response(prob_mat, class_method, class_margin):
     """
 
     (num_sample, num_class) = prob_mat.shape
-    prob_mat = normalize_matrix(prob_mat)
+    lastCat = num_class-1
+    if 'probability' in class_method.lower():
+        prob_mat = normalize_matrix(prob_mat)
     discrete_y = np.zeros((num_sample, 1), dtype=np.int)
 
     if 'probability' in class_method.lower():
-        prob_mat = np.cumsum(prob_mat, axis=1)
+        if 'ordinal' not in family_type.lower():
+            prob_mat = np.cumsum(prob_mat, axis=1)
+        else: # for ordinal
+            for indR in list(range(num_sample)):
+                for indC in list(range(num_class)):
+                    prob_mat[indR, indC] = prob_mat[indR,indC]/prob_mat[indR,lastCat]
         random_v = np.random.uniform(0, 1, [num_sample, 1])
 
         # choose the class that final response y belongs to according to the