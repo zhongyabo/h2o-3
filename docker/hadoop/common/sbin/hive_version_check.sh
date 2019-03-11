b776a2b9d8a9de638a8e6b11f320019cb6da6821
Jan Sterba <jan.sterba@h2o.ai>
2019-03-11 10:35:20 +0100

    [PUBDEV-6351] disable hive import tests on hive3

--- docker/hadoop/common/sbin/hive_version_check.sh
+++ docker/hadoop/common/sbin/hive_version_check.sh
@@ -2,18 +2,20 @@
 
 source /usr/sbin/get_hive_home.sh
 
+echo 'Determine hive version'
+
 if [ "$HIVE_HOME" = "" ]
 then
     HIVE=hive
 else
     HIVE="$HIVE_HOME/bin/hive"
 fi
-HIVE_VERSION_MAJOR=$($HIVE --version | grep -E '^Hive' | sed -E 's/Hive ([0-9])\..*/\1/')
+export HIVE_VERSION_MAJOR=$($HIVE --version | grep -E '^Hive' | sed -E 's/Hive ([0-9])\..*/\1/')
 if [ "$HIVE_VERSION_MAJOR" = "2" ] || [ "$HIVE_VERSION_MAJOR" = "3" ]
 then
     echo "Detected Hive version 2 or greater"
-    exit 0
+    export HIVE_DIST_ENABLED=true
 else
     echo "No hive or Hive 1.x or 0.x"
-    exit 1
+    export HIVE_DIST_ENABLED=false
 fi
